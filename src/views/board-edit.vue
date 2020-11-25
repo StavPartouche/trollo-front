63271d8a41a262d73a2c41cb6f1602e7af4c82a0
Stav Partouche <stave1995@gmail.com>
2020-11-25 09:27:14 +0200

    board edit

---
+++ src/views/board-edit.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>edit board</h1>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'board-edit',
+  components: {
+    
+  }
+}
+</script>
--- src/views/board-list.vue
+++ src/views/board-list.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
-    <h1>Boards</h1>
+    <ul>
+      <li v-for="board in boards" :key="board._id">
+        <router-link :to="'/board/' + board._id" >{{board.name}}</router-link>
+      </li>
+      <li>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 
-
+import { boardService } from '../services/board.service.js'
 
 export default {
   name: 'board-list',
-  components: {
+  data(){
+    return{
+      boards: [{
+        _id: 'ddfdfodiji',
+        name: 'test'
+      }],
+      newBoard: null
+    }
+  },
+  methods:{
+
+  },
+  created(){
+    this.newBoard = boardService.getEmptyBoard()
+    console.log("🚀 ~ file: board-list.vue ~ line 25 ~ created ~ this.newBoard", this.newBoard)
     
   }
 }
