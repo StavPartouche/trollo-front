16870dd0e0cca862f30cb0045297347c472eef92
Maya Malik <maya28892@gmail.com>
2020-11-29 17:33:57 +0200

    task

--- src/views/board-edit.vue
+++ src/views/board-edit.vue
@@ -282,7 +282,7 @@ export default {
     });
     this.board = JSON.parse(JSON.stringify(board));
     eventBusService.$emit("boardBgc", this.board.style);
-    this.currTask = this.board.lists[0].tasks[0]
+    // this.currTask = this.board.lists[0].tasks[0]
   },
 };
 </script>
