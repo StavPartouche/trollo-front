ba38e7a7100e7be13a8c4fe92b6c0ddce19c35ee
Maya Malik <maya28892@gmail.com>
2020-12-01 12:15:37 +0200

    disablePage

--- src/app.vue
+++ src/app.vue
@@ -4,6 +4,12 @@
     class="flex-column app-container background-image"
     :style="background"
   >
+    <div
+      @click="closeDisable"
+      v-if="disablePage.isDisable"
+      class="disable-page-container"
+      :style="{ 'z-index': disablePage.zIndex }"
+    ></div>
     <app-header />
     <router-view />
   </div>
@@ -12,8 +18,8 @@
 <script>
 import appHeader from "../src/cmps/app-header.cmp";
 import { eventBusService } from "./services/eventBus.service";
-import socket from '@/services/socket.service';
-import io from 'socket.io-client';
+import socket from "@/services/socket.service";
+import io from "socket.io-client";
 
 export default {
   name: "app",
@@ -23,7 +29,19 @@ export default {
       isBgcColor: false,
       bgcImg: "",
       bgcColor: "",
+      disablePage: {
+        isDisable: false,
+        zIndex: 1,
+        to: null
+      },
     };
+  },
+  methods: {
+    closeDisable() {
+      this.disablePage.isDisable = false;
+      eventBusService.$emit(`disablePage-${this.disablePage.to}`)
+      console.log('closeDisable');
+    },
   },
   computed: {
     background() {
@@ -31,16 +49,16 @@ export default {
         return {
           backgroundImage: `url(${require(`@/styles/assets/board-background-imgs/${this.bgcImg}`)})`,
         };
-      }else if (this.isBgcColor){
-        return{
-          backgroundColor: `${this.bgcColor}`
-        }
+      } else if (this.isBgcColor) {
+        return {
+          backgroundColor: `${this.bgcColor}`,
+        };
       }
       return "";
     },
     loggedInUser() {
       return this.$store.getters.loggedInUser;
-    }
+    },
   },
   watch: {
     $route(to, from) {
@@ -55,9 +73,9 @@ export default {
     },
   },
   created() {
-    const user = this.loggedInUser || {userName: 'guest'};
-		socket.setup(user);
-		socket.emit('user connect', user)
+    const user = this.loggedInUser || { userName: "guest" };
+    socket.setup(user);
+    socket.emit("user connect", user);
     this.$store.dispatch({
       type: "loadUsers",
     });
@@ -70,23 +88,29 @@ export default {
     //   this.bgcImg = "home1.jpg";
     // }
     eventBusService.$on("boardBgc", (bgc) => {
-      if(!bgc.url){
+      if (!bgc.url) {
         this.isBgcImg = false;
         this.isBgcColor = false;
       }
-      if (bgc.url==='color'){
+      if (bgc.url === "color") {
         this.isBgcImg = false;
         this.isBgcColor = true;
         this.bgcColor = bgc.backgroundColor;
-      } else{
+      } else {
         this.isBgcImg = true;
         this.isBgcColor = false;
         this.bgcImg = bgc.url;
       }
     });
+    eventBusService.$on("disablePage", (info) => {
+      this.disablePage.zIndex = info.zIndex ? info.zIndex : 1;
+      this.disablePage.isDisable = true;
+      this.disablePage.to = info.to
+    });
   },
   destroyed() {
-		socket.terminate();
+    eventBusService.$off("boardBgc");
+    socket.terminate();
   },
   components: {
     appHeader,
