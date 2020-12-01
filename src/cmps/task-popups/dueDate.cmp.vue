f51e91e37373cbe963823e30f57a884fce7013fb
Stav Partouche <stave1995@gmail.com>
2020-12-01 15:11:08 +0200

    commit

--- src/cmps/task-popups/dueDate.cmp.vue
+++ src/cmps/task-popups/dueDate.cmp.vue
@@ -3,6 +3,12 @@
     <h3 class="task-popup-header">dueDate</h3>
     <button @click="closePopup" class="exit-popup-btn">X</button>
     <input @input="updateDueDate" type="date" v-model="date" />
+    <!-- <el-date-picker
+      @input="updateDueDate"
+      v-model="date"
+      type="date"
+      placeholder="Pick a day">
+    </el-date-picker> -->
   </div>
 </template>
 
