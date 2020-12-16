34ac0ef17f520876c181f8a36b593f43117e2423
Inbar Tamir <inbartamir92@gmail.com>
2020-12-16 20:31:18 +0200

    stop sw

--- src/services/img-upload.service.js
+++ src/services/img-upload.service.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
-export const uploadImg = async (ev) =>{
-    // Defining our variables
-    const UPLOAD_PRESET = 'trello_pre' // Insert yours
-    const CLOUD_NAME = 'trello' // Insert yours
-    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+export const uploadImg = async (ev) => {
+    const UPLOAD_PRESET = 'trello_pre';
+    const CLOUD_NAME = 'trello';
+    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
     const FORM_DATA = new FormData();
     // Building the request body
-    FORM_DATA.append('file', ev.target.files[0])
-    FORM_DATA.append('upload_preset',UPLOAD_PRESET)
+    FORM_DATA.append('file', ev.target.files[0]);
+    FORM_DATA.append('upload_preset', UPLOAD_PRESET);
     // Sending a post method request to Cloudniarys' API
     try {
-        const res = await axios.post(UPLOAD_URL, FORM_DATA)
+        const res = await axios.post(UPLOAD_URL, FORM_DATA);
         return res.data;
-    } catch(err) {
-        console.error('ERROR!', err)
+    } catch (err) {
+        console.error('ERROR!', err);
     }
-}
+};
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import homePage from '@/views/home-page.vue'
 
-describe('HelloWorld.vue', () => {
+describe('home-page.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(homePage, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
